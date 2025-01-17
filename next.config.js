c2601bbf44bfc96ec970965bc586e92629fda6b2
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-17 11:00:50 +0000

    l

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '', // If your site is at username.github.io, leave empty
-    // basePath: '/repo-name', // If your site is at username.github.io/repo-name
+    basePath: '',
+    assetPrefix: '/', // Add this line// basePath: '/repo-name', // If your site is at username.github.io/repo-name
     images: {
         unoptimized: true,
     },
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        "app/**/*.{ts,tsx}",
+        "components/**/*.{ts,tsx}"],
     theme: {
         container: {
             center: true,
