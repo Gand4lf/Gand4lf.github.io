31c6b6978f886e354f5855a178402540c1cade1c
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-17 11:12:06 +0000

    n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '',
-    assetPrefix: '/', // Add this line// basePath: '/repo-name', // If your site is at username.github.io/repo-name
+
     images: {
         unoptimized: true,
     },
