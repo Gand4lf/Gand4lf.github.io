dbbd01f403b71cd50973fc0563b00c54e6d81142
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-17 02:09:41 +0000

    l

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/Gand4lf.github.io',
     images: {
         unoptimized: true,
     },
-}
+    assetPrefix: '/gand4lf.github.io/',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
