3b89e4c8a97361ffc6ba0cafaf8667ea1fe52e05
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-17 01:23:10 +0000

    e

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    title: string
+    date: string
+    description: string
+    technologies: string[]
+    longDescription?: string
+    images?: string[]
+    link?: string
+} 
