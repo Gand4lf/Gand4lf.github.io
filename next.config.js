f320f8b680f73c65f325eebef1d58c55a8547d47
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2025-01-17 01:54:50 +0000

    p

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    output: 'export',
     images: {
         unoptimized: true,
     },
-    distDir: 'docs',
+    assetPrefix: '/gand4lf.github.io/',
+    basePath: '/gand4lf.github.io',
 };
 
 module.exports = nextConfig;
